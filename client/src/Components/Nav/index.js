b4b948945d38aad60e32d40a6212d7553b720e45
Jadan Morrow <jadanmorrow@outlookcom>
2020-06-22 18:20:35 -0600

    changing button color

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -11,14 +11,14 @@ function Nav() {
         <ul className="navbar-nav">
           <li className="nav-item" id="home">
             <a className="nav-link" href="/">
-              <button type="button" className="btn btn-info text-white">
+              <button type="button" className="btn btn-light text-black">
                 Search Books
               </button>
             </a>
           </li>
           <li className="nav-item" id="report">
             <a className="nav-link" href="/saved">
-              <button type="button" className="btn btn-danger text-white">
+              <button type="button" className="btn btn-secondary text-black">
                 Saved Books
               </button>
             </a>
